e9982784d4857b1cf8ff3825da31ac8188fcab42
Niyush-04 <mauryapradip04@gmail.com>
2026-06-30 14:56:35 +0530

    spring: Ship firmware images

--- Android.mk
+++ Android.mk
@@ -3,3 +3,25 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),spring)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,43e36b1f65fdceed5bc321efdfc88746ab7ba064)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,24fedcaf06eec9c623f01d8a976848d3fbc7d7ba)
+$(call add-radio-file-sha1-checked,radio/countrycode.img,1a8d43b56233403d592d74aca534ec9ad73c9c46)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,bd30fdbd719bea349e4ab64b9ad6529aedcd4d1d)
+$(call add-radio-file-sha1-checked,radio/dsp.img,099465b6d8615d24806ec34f116bc50d75a54936)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,b6ecac8c96e775c354505069e22cd7ef8f73c69c)
+$(call add-radio-file-sha1-checked,radio/hyp.img,bfddcf72a4374d4da95113f1cc015688d716e6a6)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,6a9361e3e82ec41b4cd042c7d2b9ba2dec933ab8)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,7da5f38bcf566f2fb87b28be13af957f2d6f24ed)
+$(call add-radio-file-sha1-checked,radio/modem.img,529ba0c8c95fc498407b8ff77a0fabec1edcafbc)
+$(call add-radio-file-sha1-checked,radio/pvmfw.img,5602476f7b21635741f46d2e14d9da9703b32121)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,ece9f45db446a1f67c66ebe2e6022b89df398049)
+$(call add-radio-file-sha1-checked,radio/rpm.img,7501a9299e3015c4f8a39c02127d8c398270f795)
+$(call add-radio-file-sha1-checked,radio/tz.img,4820f7e5bfb94d839db471626af0f2154832b629)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,53813d9ce0f4b8f103ebcf89d99b3e5781831bc4)
+$(call add-radio-file-sha1-checked,radio/xbl.img,38b7ec62976b6fd0aa9a3c875efde0e0cd222356)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,d1890d3c67ec2c004f2548f49830c74f35dfd391)
+
+endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,3 +1,22 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
+
+AB_OTA_PARTITIONS += \
+    abl \
+    bluetooth \
+    countrycode \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    pvmfw \
+    qupfw \
+    rpm \
+    tz \
+    uefisecapp \
+    xbl \
+    xbl_config
